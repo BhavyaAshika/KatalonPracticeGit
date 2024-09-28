b342309e6981baa5d2bdea1683e4a58cf7ffaa10
GR644HV <GR644HV@XW5CG3415Q46.ey.net>
2024-09-28 19:52:42 +0530

    latest

---
+++ Test Suites/Validate Welcome.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Validate Welcome</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ac3bd1ff-52f6-42d0-a256-5fef6eaa792a</testSuiteGuid>
+   <testCaseLink>
+      <guid>4a3a4931-2162-4c28-a579-e329a21963cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Validate Welcome</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
